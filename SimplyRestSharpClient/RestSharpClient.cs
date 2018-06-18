66a2f3c1078b69b5bc95a3961c4f30e272c28a33
Harold Barros <haroldbarros@gmail.com>
2018-06-18 13:37:44 -0300

    Alteracoes e melhorias

--- SimplyRestSharpClient/RestSharpClient.cs
+++ SimplyRestSharpClient/RestSharpClient.cs
@@ -1,9 +1,10 @@
 ﻿using RestSharp;
+using System;
 using System.Threading.Tasks;
 
 namespace SimplyRestSharpClient
 {
-    public class RestSharpClient
+	public class RestSharpClient:IDisposable
     {
         private string baseUrl;
 
@@ -12,50 +13,96 @@ namespace SimplyRestSharpClient
             this.baseUrl = _baseUrl;
         }
 
-        public async Task<IRestResponse<T>> RestExecuteAsync<T>(string resource, object param, Method method) where T : new()
-        {
-            var client = new RestClient(baseUrl);
-            var request = new RestRequest(resource, method);
-            request.AddObject(param);
-            var asyncHandler = await client.ExecuteTaskAsync<T>(request);
-
-            return asyncHandler;
-        }
-
-        public async Task<IRestResponse<T>> PostAsync<T>(string resource, object param) where T : new()
+		public async Task<IRestResponse<T>> PostAsync<T>(string resource, object param = null) where T : new()
         {
             return await RestExecuteAsync<T>(resource, param, Method.POST);
         }
 
-        public async Task<IRestResponse<T>> GetAsync<T>(string resource, object param) where T : new()
+		public async Task<IRestResponse<T>> GetAsync<T>(string resource, object param = null) where T : new()
         {
+			
             return await RestExecuteAsync<T>(resource, param, Method.GET);
         }
 
-        public async Task<IRestResponse<T>> PutAsync<T>(string resource, object param) where T : new()
+		public async Task<IRestResponse<T>> PutAsync<T>(string resource, object param = null) where T : new()
         {
             return await RestExecuteAsync<T>(resource, param, Method.PUT);
         }
 
-        public async Task<IRestResponse<T>> DeleteAsync<T>(string resource, object param) where T : new()
+		public async Task<IRestResponse<T>> DeleteAsync<T>(string resource, object param = null) where T : new()
         {
             return await RestExecuteAsync<T>(resource, param, Method.DELETE);
+        }
+
+		public async Task<IRestResponse<T>> RestExecuteAsync<T>(string resource, object param, Method method) where T : new()
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(resource, method);
+            if (param != null)
+                request.AddObject(param);
+            var asyncHandler = await client.ExecuteTaskAsync<T>(request);
+
+            return asyncHandler;
         }
 
         public async Task<IRestResponse> RestExecuteAsync(string resource, object param, Method method)
         {
             var client = new RestClient(baseUrl);
             var request = new RestRequest(resource, method);
-            request.AddObject(param);
+			if (param != null)
+                request.AddObject(param);
             var asyncHandler = await client.ExecuteTaskAsync(request);
 
             return asyncHandler;
         }
 
-        public async Task<IRestResponse> PostAsync(string resource, object param)
+
+		public async Task<IRestResponse> PostAsync(string resource, object param = null)
         {
             return await RestExecuteAsync(resource, param, Method.POST);
         }
+        
+
+
+		#region IDisposable
+
+		// Flag: Has Dispose already been called?
+        bool disposed = false;
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                //
+
+
+            }
+
+            // Free any unmanaged objects here.
+            //
+
+
+            disposed = true;
+        }
+
+		~RestSharpClient()
+        {
+            Dispose(false);
+        }
+
+		#endregion IDisposable
 
     }
+
 }
